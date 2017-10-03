ea5ccd222c3fa3ed09d3f6338febc1a2d69a2249
djtrujillo <danny.j.trujillo@gmail.com>
2017-10-03 15:31:04 -0600

    Merchant can find by and find all by id

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
+
   namespace :api do
     namespace :v1 do
-      resources :merchants, only: [:index]
+
+      namespace :merchants do
+        get '/find', to: "find#show"
+        get '/find_all', to: "find#index"
+      end
+
+      resources :merchants, only: [:index, :show]
+
+
+
     end
   end
-
 end
--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -12,4 +12,39 @@ describe "Merchants API" do
 
     expect(merchants.count).to eq(3)
   end
+
+  it "can get one merchant by its id" do
+    id = create(:merchant).id
+
+    get "/api/v1/merchants/#{id}"
+
+    merchant = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(merchant["id"]).to eq(id)
+  end
+
+  it "can find first instance by id" do
+    id = create(:merchant).id
+
+    get "/api/v1/merchants/find?id=#{id}"
+
+    merchant = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(merchant["id"]).to eq(id)
+  end
+
+  it "can find all instances by name" do
+    name = create(:merchant).name
+
+    get "/api/v1/merchants/find_all?name=#{name}"
+
+    merchant = JSON.parse(response.body)
+
+
+    expect(response).to be_success
+    expect(merchant.first["name"]).to eq(name)
+  end
+
 end
