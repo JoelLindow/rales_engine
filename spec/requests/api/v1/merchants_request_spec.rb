a82a5203e498aecdb8deb4dd39b15604108ef526
djtrujillo <danny.j.trujillo@gmail.com>
2017-10-03 14:10:50 -0600

    First test merchant index passing

---
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe "Merchants API" do
+  it "sends a list of merchants" do
+    create_list(:merchant, 3)
+
+    get '/api/v1/merchants'
+
+    expect(response).to be_success
+
+    merchants = JSON.parse(response.body)
+
+    expect(merchants.count).to eq(3)
+  end
+end
