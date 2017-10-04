e9c1c48f7bd777eab68a09c2f601711f15ef9dd1
djtrujillo <danny.j.trujillo@gmail.com>
2017-10-03 18:53:51 -0600

    Transaction record endpoints working

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,18 @@ Rails.application.routes.draw do
 
 
 
+      namespace :transactions do
+        get '/random', to: "random#show"
+        get '/find', to: "find#show"
+        get '/find_all', to: "find#index"
+
+      end
+
+
+      resources :transactions, only: [:index, :show]
+
+
+
 
     end
   end
