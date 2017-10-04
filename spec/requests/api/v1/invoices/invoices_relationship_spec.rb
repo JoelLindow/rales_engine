bcb5008c24b13698fce86cce54feb69a2a13f15e
JoelLindow <joellindow@gmail.com>
2017-10-03 18:59:35 -0600

    customers testing working

--- spec/requests/api/v1/invoices/invoices_relationship_spec.rb
+++ spec/requests/api/v1/invoices/invoices_relationship_spec.rb
@@ -54,4 +54,25 @@ describe "invoices relationships" do
 
     expect(items.count).to eq(3)
   end
+
+  it "returns customer" do
+    customer = create(:customer)
+    merchant = create(:merchant)
+    invoice = create(:invoice, customer_id: customer.id, merchant_id: merchant.id)
+    item1, item2, item3 = create_list(:item, 3, merchant_id: merchant.id)
+    create(:invoice_item, item_id: item1.id, invoice_id: invoice.id)
+    create(:invoice_item, item_id: item2.id, invoice_id: invoice.id)
+    create(:invoice_item, item_id: item3.id, invoice_id: invoice.id)
+    invoice_id = invoice.id
+
+    get "/api/v1/invoices/#{invoice_id}/customer"
+
+    expect(response).to be_success
+
+    customer_json = JSON.parse(response.body)
+
+    expect(customer_json["first_name"]).to eq(customer.first_name)
+  end
+
+
 end
