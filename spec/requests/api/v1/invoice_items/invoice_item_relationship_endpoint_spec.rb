0a0661859dd5043e582bf3e3b5d6cf4d7b66a84e
djtrujillo <danny.j.trujillo@gmail.com>
2017-10-04 14:13:12 -0600

    returns associated item

--- spec/requests/api/v1/invoice_items/invoice_item_relationship_endpoint_spec.rb
+++ spec/requests/api/v1/invoice_items/invoice_item_relationship_endpoint_spec.rb
@@ -17,6 +17,19 @@ describe "Invoice Items Relationships" do
   end
 
   it "returns the associated item" do
+    customer = create(:customer)
+    merchant = create(:merchant)
+    item = create(:item, merchant_id: merchant.id)
+    invoice = create(:invoice, customer_id: customer.id, merchant_id: merchant.id)
+    invoice_item = create(:invoice_item, invoice_id: invoice.id, item_id: item.id)
+
+    get "/api/v1/invoice_items/#{invoice_item.id}/item"
+
+    item_json = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(invoice_item.item).to eq(item)
+
 
   end
 end
