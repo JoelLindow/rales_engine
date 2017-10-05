5cb606b77842cd7b042eb4621fa802bf1c31789a
djtrujillo <danny.j.trujillo@gmail.com>
2017-10-05 15:32:21 -0600

    most revenue for items

---
+++ app/controllers/api/v1/items/most_revenue_controller.rb
@@ -0,0 +1,13 @@
+module Api
+  module V1
+    module Items
+      class MostRevenueController < ApplicationController
+
+        def index
+          render json: Item.most_revenue(params[:quantity])
+        end
+
+      end
+    end
+  end
+end
